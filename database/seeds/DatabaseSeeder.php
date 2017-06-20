f20f7337ca21be5623bc7de12ea0534f9f854fd4
Leonardo Cherubini <Leonardo Cherubini>
2017-06-19 21:31:50 -0300

    Refactoring Group Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(AnswersTableSeeder::class);
+        $this->call(GroupsTableSeeder::class);
     }
 }
